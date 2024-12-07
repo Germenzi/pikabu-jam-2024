04868429f7b2fa0c97d725247028789a9d3de583
sekhmych <sekhmych@gmail.com>
2024-12-07 14:15:01 +0900

    Add level_1 and level loading in game.tscn

--- namespaces/ScenesNamespace.gd
+++ namespaces/ScenesNamespace.gd
@@ -4,3 +4,4 @@ class_name ScenesNamespace
 
 const PLAYER_BULLET_FILEPATH : String = "res://player/player_bullet.tscn"
 const PLAYER_BOMB_FILEPATH : String = "res://player/player_bomb.tscn"
+const PLAYER_FILEPATH : String = "res://player/Player.tscn"
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,8 +1,48 @@
 extends Node2D
 
+signal player_ready
+
+enum Levels {
+	Level1
+}
+
+@export_category("player")
+@export var player_x : int
+@export var player_y : int
+
+@export_category("level")
+@export var level : PackedScene
+
+@export_category("camera")
+@export var camera_left_limit : int = 0
+@export var camera_right_limit : int = 1000
+@export var camera_top_limit : int = 0
+@export var camera_bottom_limit : int = 720
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Camera2D.position.x = 0
+	$Camera2D.position.y = 0
+	$Camera2D.limit_left = camera_left_limit
+	$Camera2D.limit_right = camera_right_limit
+	$Camera2D.limit_top = camera_top_limit
+	$Camera2D.limit_bottom = camera_bottom_limit
+	
+	add_child(level.instantiate())
+	
+	var player = preload(ScenesNamespace.PLAYER_FILEPATH)
+	player = player.instantiate()
+	player.position.x = player_x
+	player.position.y = player_y
+	add_child(player)
+	$Camera2D.player = player
+	player_ready.emit()
+	
+	if not GuiTransitions.in_transition():
+		await(get_tree().create_timer(2).timeout)
+		print("HIED")
+		GuiTransitions.hide("LoadingLevel")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

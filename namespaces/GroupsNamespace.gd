30971f21b2fc724491e2cee59c31aeed243e68d3
Nikita\Nick <kineeetik@gmail.com>
2024-12-07 21:36:44 +0300

    Add fat cockroach

--- namespaces/GroupsNamespace.gd
+++ namespaces/GroupsNamespace.gd
@@ -5,3 +5,6 @@ class_name GroupsNamespace
 const PLAYER : String = "player"
 const DESTRUCTABLE_OBJECT : String = "destructable"
 const SOLID_OBSTACLE : String = "solid_object"
+const COCKROACH_TARGET : String = "cockroach_target"
+const COOKIE : String = "cookie"
+const DAMAGABLE : String = "damagable"
--- namespaces/ScenesNamespace.gd
+++ namespaces/ScenesNamespace.gd
@@ -5,3 +5,4 @@ class_name ScenesNamespace
 const PLAYER_BULLET_FILEPATH : String = "res://player/player_bullet.tscn"
 const PLAYER_BOMB_FILEPATH : String = "res://player/player_bomb.tscn"
 const PLAYER_FILEPATH : String = "res://player/Player.tscn"
+const COCKROACH_BULLET_FILEPATH : String = "res://cockroaches/cockroach_bullet.tscn"
